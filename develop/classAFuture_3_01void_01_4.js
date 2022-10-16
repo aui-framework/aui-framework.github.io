a6f06fe94ee2ea92287574d32ef5c91e7bfb3f8b
Alex2772 <Alex2772@users.noreply.github.com>
2022-10-16 02:18:35 +0000

    deploy: aui-framework/aui@0588ea47f71c3fde3442166a850fdad2d89796d8

--- develop/classAFuture_3_01void_01_4.js
+++ develop/classAFuture_3_01void_01_4.js
@@ -1,6 +1,6 @@
 var classAFuture_3_01void_01_4 =
 [
-    [ "onError", "classAFuture_3_01void_01_4.html#a8fa17084bd5cb6dac8808c5904e1cbdf", null ],
-    [ "onSuccess", "classAFuture_3_01void_01_4.html#a17c1a9b7d89a651d144ae82f7ba06b8f", null ],
+    [ "onError", "classAFuture_3_01void_01_4.html#a5b99b885a63bfc96085b894b248f5917", null ],
+    [ "onSuccess", "classAFuture_3_01void_01_4.html#a5312213cf82b1e04f0e48be12e0131b2", null ],
     [ "supplyException", "classAFuture_3_01void_01_4.html#ad8f671c9cd3307d2e83b3f199cb0a4d2", null ]
 ];
