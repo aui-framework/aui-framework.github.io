57bb51ba77b7ba119c27435802645c78368c627e
Alex2772 <Alex2772@users.noreply.github.com>
2023-08-16 17:20:16 +0000

    deploy: aui-framework/aui@18605c7c527511af665102e92efa9ea290acff2a

--- develop/search/variables_b.js
+++ develop/search/variables_b.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['textchanged_0',['textChanged',['../classAAbstractTypeableView.html#a4addebde2ee640e32b78040f71f2d107',1,'AAbstractTypeableView']]],
   ['textchanging_1',['textChanging',['../classAAbstractTypeableView.html#ad5412b86c537ee74c130c7f3623ec09c',1,'AAbstractTypeableView']]],
-  ['triggerclick_2',['triggerClick',['../structAPointerReleasedEvent.html#a1721df1466c1635ad2fc30e30fbf66a0',1,'APointerReleasedEvent']]]
+  ['timefordoubleclick_2',['timeForDoubleClick',['../classABaseWindow.html#ab1bf4d61c373685191ba100e5a360f38',1,'ABaseWindow']]],
+  ['triggerclick_3',['triggerClick',['../structAPointerReleasedEvent.html#a1721df1466c1635ad2fc30e30fbf66a0',1,'APointerReleasedEvent']]]
 ];
