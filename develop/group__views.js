24d37a04692a59dfc5857136e75c4f1a6397a64e
Alex2772 <Alex2772@users.noreply.github.com>
2023-10-25 23:33:31 +0000

    deploy: aui-framework/aui@29d43c5443829ffd666a8a73ce0e433830394af6

--- develop/group__views.js
+++ develop/group__views.js
@@ -6,7 +6,7 @@ var group__views =
     [ "ADesktop", "namespaceADesktop.html", null ],
     [ "APointerIndex", "classAPointerIndex.html", [
       [ "button", "classAPointerIndex.html#a49a88f55f3c0145e68138f593a285cb6", null ],
-      [ "finger", "classAPointerIndex.html#abcc86fc123d626ab24ae9cd3a621b8bb", null ],
+      [ "finger", "classAPointerIndex.html#a45f8137400387a26ebd36a09971deff2", null ],
       [ "isButton", "classAPointerIndex.html#a587505abb5e0de9f6343d12713b7d2dd", null ],
       [ "isFinger", "classAPointerIndex.html#a050825608f15d569612ed87c0379a038", null ]
     ] ],
