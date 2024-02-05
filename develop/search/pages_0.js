94adde23a197d96c4cf52b33cbca653f0751c0bf
Alex2772 <Alex2772@users.noreply.github.com>
2024-02-05 05:42:48 +0000

    deploy: aui-framework/aui@919aabf13ccc6a782ffe15c86a67cb62d848a2b3

--- develop/search/pages_0.js
+++ develop/search/pages_0.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['aui_20boot_0',['AUI Boot',['../md_docs_AUI_Boot.html',1,'']]],
   ['aui_20command_20line_20arguments_1',['AUI Command Line Arguments',['../md_docs_AUI_command_line_arguments.html',1,'']]],
-  ['aui_20framework_2',['AUI Framework',['../index.html',1,'']]],
-  ['aui_2ebuild_3',['aui.build',['../md_docs_AUI_configure_flags.html',1,'']]],
+  ['aui_20configure_20flags_2',['AUI configure flags',['../md_docs_AUI_configure_flags.html',1,'']]],
+  ['aui_20framework_3',['AUI Framework',['../index.html',1,'']]],
   ['aui_5fapp_4',['aui_app',['../md_docs_aui_app.html',1,'']]],
   ['aui_5fexecutable_5',['aui_executable',['../md_docs_aui_executable.html',1,'']]]
 ];
