f2b93e594e30c02e0d8b120d2a434e5a86abd059
Alex2772 <Alex2772@users.noreply.github.com>
2024-03-19 15:10:38 +0000

    deploy: aui-framework/aui@4f351f59f8f42b350e9595b993e690e763fa1714

---
+++ v3.0.25/classACursor.js
@@ -0,0 +1,9 @@
+var classACursor =
+[
+    [ "Custom", "structACursor_1_1Custom.html", null ],
+    [ "System", "classACursor.html#ab9abb63ef7f5d7d771299ab2c80deb03", [
+      [ "DEFAULT", "classACursor.html#ab9abb63ef7f5d7d771299ab2c80deb03a80f892002d6dcf4cb956f8fac412a4b6", null ],
+      [ "POINTER", "classACursor.html#ab9abb63ef7f5d7d771299ab2c80deb03afd0328e163b3c7a9f659e4ec4720759c", null ],
+      [ "TEXT", "classACursor.html#ab9abb63ef7f5d7d771299ab2c80deb03a33e3afd15b2a0c9d8d808bcc7b3e4aed", null ]
+    ] ]
+];
