2be14f16add6ebf2256d1ce2f03f69a9f3df0130
Alex2772 <Alex2772@users.noreply.github.com>
2024-03-19 14:19:22 +0000

    deploy: aui-framework/aui@3404512c1b0d30302c2868dd8f65be5a66029e5f

---
+++ v3.0.24/classAPlaceholderAnimator.js
@@ -0,0 +1,4 @@
+var classAPlaceholderAnimator =
+[
+    [ "ICustomWidth", "classAPlaceholderAnimator_1_1ICustomWidth.html", null ]
+];
---
+++ v3.0.24/classATranslationAnimator.js
@@ -0,0 +1,4 @@
+var classATranslationAnimator =
+[
+    [ "doAnimation", "classATranslationAnimator.html#ab247b32895ce745d499ca92ebd956462", null ]
+];
