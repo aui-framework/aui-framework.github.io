bf30e822b6b6a11178b51649611f87d8948429dc
Alex2772 <Alex2772@users.noreply.github.com>
2024-03-26 15:49:58 +0000

    deploy: aui-framework/aui@7cb9421a55157bebe02d7efba2306fe09212726e

---
+++ v3.0.32/navtreedata.js
@@ -0,0 +1,85 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "AUI Framework", "index.html", [
+    [ "glossary", "usergroup0.html", [
+      [ "Namespace List", "namespaces.html", "namespaces" ],
+      [ "Namespace Members", "namespacemembers.html", [
+        [ "All", "namespacemembers.html", null ],
+        [ "Functions", "namespacemembers_func.html", null ]
+      ] ],
+      [ "Class List", "annotated.html", "annotated" ],
+      [ "Class Hierarchy", "hierarchy.html", "hierarchy" ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", "functions_func" ],
+        [ "Variables", "functions_vars.html", null ],
+        [ "Typedefs", "functions_type.html", null ],
+        [ "Enumerations", "functions_enum.html", null ],
+        [ "Enumerator", "functions_eval.html", null ]
+      ] ]
+    ] ],
+    [ "Getting started", "usergroup1.html", [
+      [ "Getting started with AUI", "md_docs_Getting_started_with_AUI.html", null ],
+      [ "AUI Boot", "md_docs_AUI_Boot.html", null ],
+      [ "Writing tests", "md_docs_Writing_tests.html", null ],
+      [ "Writing platform dependent code", "md_docs_Writing_platform_dependent_code.html", null ],
+      [ "Code style and recommendations", "md_docs_Code_style_and_recommendations.html", null ],
+      [ "Troubleshoot list", "md_docs_Troubleshoot_list.html", null ],
+      [ "Crosscompiling (Android and iOS)", "md_docs_Crosscompiling.html", null ]
+    ] ],
+    [ "Tweaking", "usergroup2.html", [
+      [ "AUI configure flags", "md_docs_AUI_configure_flags.html", null ],
+      [ "AUI command line arguments", "md_docs_AUI_command_line_arguments.html", null ]
+    ] ],
+    [ "Reference", "usergroup3.html", [
+      [ "All AUI Modules", "modules.html", "modules" ],
+      [ "All AUI Classes", "classes.html", null ],
+      [ "All AUI CMake commands", "usergroup4.html", [
+        [ "aui_app", "md_docs_aui_app.html", null ],
+        [ "aui_executable", "md_docs_aui_executable.html", null ],
+        [ "aui_module", "md_docs_aui_module.html", null ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"classADeque.html#ad1396ac646b307ff34e8e9d594d27181",
+"classAPipe.html#a4b86fe3324e01417be2a4b33c87e2279",
+"classATextArea_1_1TextAreaField.html#a755c9bf00449be4736faaf8096634dd9",
+"classAWindow.html#ad5576c2e3a9ce8c20ef1e5d4dedfebe7",
+"classUtil_1_1TexturePacker__Lol.html",
+"group__useful__macros.html#ga0b5c29ea5716ab0c88709f9b472a2f64",
+"structViewActionClick.html",
+"structaui_1_1dbus_1_1converter_3_01std_1_1uint8__t_01_4.html",
+"structgl_1_1impl_1_1resource.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
---
+++ v3.0.32/search/typedefs_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['args_0',['args',['../structaui_1_1member_3_01Type_07Clazz_1_1_5_08_07Args_8_8_8_08_01const_01_4.html#a2bd40a82d18d21ffb181a6d096dd56d4',1,'aui::member&lt; Type(Clazz::*)(Args...) const &gt;::args()'],['../structaui_1_1member_3_01Type_07Clazz_1_1_5_08_07Args_8_8_8_08_4.html#ac6f26e325156e1f1c93120a5b55ca85f',1,'aui::member&lt; Type(Clazz::*)(Args...)&gt;::args()']]]
+];
