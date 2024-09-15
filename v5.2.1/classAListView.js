09d01d2b411d42e49a0af3e89b3f3478ddec7b9f
Alex2772 <Alex2772@users.noreply.github.com>
2024-09-15 19:43:15 +0000

    deploy: aui-framework/aui@17e278176d454fd2a4ea72066e9efa29cfe52ccc

---
+++ v5.2.1/classAListView.js
@@ -0,0 +1,11 @@
+var classAListView =
+[
+    [ "SelectAction", "classAListView.html#a9d28394809b0d6ff7e1783e5b253ff47", [
+      [ "CLEAR_SELECTION_AND_SET", "classAListView.html#a9d28394809b0d6ff7e1783e5b253ff47adedd147810f45d4a387137a76016b121", null ],
+      [ "SET", "classAListView.html#a9d28394809b0d6ff7e1783e5b253ff47a8c52684db8f49511e9b44471716bf164", null ],
+      [ "UNSET", "classAListView.html#a9d28394809b0d6ff7e1783e5b253ff47a666a87025ab0d8965e221050c8948001", null ],
+      [ "TOGGLE", "classAListView.html#a9d28394809b0d6ff7e1783e5b253ff47a1252c45651289c0b553a30d27bb5ae9b", null ]
+    ] ],
+    [ "onGesture", "classAListView.html#a1fe6ab723b7168060e837126b374043a", null ],
+    [ "selectItem", "classAListView.html#a7369cc389a88274c38c4f51693734b0f", null ]
+];
---
+++ v5.2.1/structAViewContainer_1_1PointerEventsMapping.js
@@ -0,0 +1,4 @@
+var structAViewContainer_1_1PointerEventsMapping =
+[
+    [ "isBlockClicksWhenPressed", "structAViewContainer_1_1PointerEventsMapping.html#a53bbbba56d7255476a756b50cd5eef08", null ]
+];
