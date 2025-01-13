87b4d1d2a53f91c02318cdcb64fee5bda03a30eb
Alex2772 <Alex2772@users.noreply.github.com>
2025-01-13 09:02:44 +0000

    deploy: aui-framework/aui@0f60c12c588c133c8a15a2f0e2994d2012741c98

--- develop/search/all_12.js
+++ develop/search/all_12.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['objectpath_0',['ObjectPath',['../classaui_1_1dbus_1_1ObjectPath.html',1,'aui::dbus']]],
-  ['observing_20changes_1',['Observing changes',['../group__property__system.html#autotoc_md155',1,'Observing changes'],['../group__property__system.html#autotoc_md156',1,'Observing changes'],['../group__property__system.html#autotoc_md157',1,'Observing changes']]],
+  ['observing_20changes_1',['Observing changes',['../group__property__system.html#autotoc_md156',1,'Observing changes'],['../group__property__system.html#autotoc_md157',1,'Observing changes'],['../group__property__system.html#autotoc_md158',1,'Observing changes']]],
   ['of_2',['of',['../structaui_1_1sequence__traits_1_1of.html',1,'aui::sequence_traits']]],
   ['ole_3',['Ole',['../classOle.html',1,'']]],
   ['omitting_20view_20property_4',['ADataBindingDefault for omitting view property',['../group__property__system.html#ADataBindingDefault_for_omitting_view_property',1,'']]],
@@ -46,9 +46,9 @@ var searchData=
   ['operator_5b_5d_43',['operator[]',['../classAListModel.html#ac65199711ed9f613d8b3a9d4d385a1d0',1,'AListModel']]],
   ['or_44',['Or',['../structaui_1_1valid_1_1impl_1_1Or.html',1,'aui::valid::impl']]],
   ['or_20abutton_20s_45',['Select all ALabel&apos;s or AButton&apos;s',['../group__ass.html#autotoc_md16',1,'']]],
-  ['or_20exception_46',['Assert or exception?',['../md_docs_2Code_01style_01and_01recommendations.html#autotoc_md115',1,'']]],
+  ['or_20exception_46',['Assert or exception?',['../md_docs_2Code_01style_01and_01recommendations.html#autotoc_md114',1,'']]],
   ['origin_47',['origin',['../structAScrollEvent.html#afb585641aa999c42077063ccf83c6ffe',1,'AScrollEvent']]],
-  ['other_48',['Other',['../md_docs_2Macros.html#autotoc_md137',1,'']]],
+  ['other_48',['Other',['../md_docs_2Macros.html#autotoc_md138',1,'']]],
   ['out_49',['out',['../classPipe.html#ad191448b8da4dbcde894f89fb5dd3a3b',1,'Pipe']]],
   ['overlaycolor_50',['overlayColor',['../structass_1_1BackgroundImage.html#ae84e6aecde1d3970a6b3504c842419eb',1,'ass::BackgroundImage']]],
   ['overview_51',['UI Building Overview',['../md_docs_2UI_01Building_01Overview.html',1,'']]]
