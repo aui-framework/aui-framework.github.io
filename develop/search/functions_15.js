d25150029d943a60cd1e6dfdb09a85b2d8f84ed9
Alex2772 <Alex2772@users.noreply.github.com>
2025-03-29 03:58:42 +0000

    deploy: aui-framework/aui@b653efa880e949a0542bd99f2f80543da4364c69

--- develop/search/functions_15.js
+++ develop/search/functions_15.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['value_0',['value',['../classACommandLineArgs.html#a3393e32c6e2904ee2955135511801812',1,'ACommandLineArgs::value()'],['../classANumberPicker.html#ac7d73e19178ba3e788205a05b079f08d',1,'ANumberPicker::value()']]],
+  ['value_0',['value',['../class__.html#afca66562283bb9686c3c8a95bae5a42e',1,'_::value()'],['../classACommandLineArgs.html#a3393e32c6e2904ee2955135511801812',1,'ACommandLineArgs::value()'],['../classANumberPicker.html#ac7d73e19178ba3e788205a05b079f08d',1,'ANumberPicker::value()']]],
   ['valuename_1',['valueName',['../classAEnumerate.html#ab5bff42b9ce66c1a398b556b6fa1e52e',1,'AEnumerate']]],
   ['valueor_2',['valueOr',['../classAOptional.html#a71c3dc1b2ba9b9017348e471833b5d2c',1,'AOptional']]],
   ['valueorexception_3',['valueOrException',['../classAOptional.html#ac2f1093971087a13def94bbd620b7735',1,'AOptional::valueOrException(const char *message=&quot;empty optional&quot;)'],['../classAOptional.html#a22e06483655aac11d8d1307086d5ba34',1,'AOptional::valueOrException(const char *message=&quot;empty optional&quot;) const']]],
