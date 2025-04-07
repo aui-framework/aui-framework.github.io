ac255aef38776cb7034653f27f4dc28bc58590d7
Alex2772 <Alex2772@users.noreply.github.com>
2025-04-07 04:44:45 +0000

    deploy: aui-framework/aui@7bcb9248ab8c916bd432848caa3e69844fd5ef1c

--- develop/search/all_1.js
+++ develop/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['1_0',['1',['../md_docs_2AUI_01Boot.html#autotoc_md66',1,'AUIB_VALIDATION_LEVEL 1'],['../md_docs_2AUI_01Boot.html#autotoc_md69',1,'Case 1']]]
+  ['1_0',['1',['../md_docs_2AUI_01Boot.html#autotoc_md67',1,'AUIB_VALIDATION_LEVEL 1'],['../md_docs_2AUI_01Boot.html#autotoc_md70',1,'Case 1']]]
 ];
--- develop/search/all_2.js
+++ develop/search/all_2.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['2_0',['2',['../md_docs_2AUI_01Boot.html#autotoc_md67',1,'AUIB_VALIDATION_LEVEL 2'],['../md_docs_2AUI_01Boot.html#autotoc_md70',1,'Case 2']]]
+  ['2_0',['2',['../md_docs_2AUI_01Boot.html#autotoc_md68',1,'AUIB_VALIDATION_LEVEL 2'],['../md_docs_2AUI_01Boot.html#autotoc_md71',1,'Case 2']]]
 ];
