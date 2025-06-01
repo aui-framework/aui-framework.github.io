a9291a392ae724c02aadaae969631f19231a185c
Alex2772 <Alex2772@users.noreply.github.com>
2025-06-01 17:35:23 +0000

    deploy: aui-framework/aui@3edd3f741affee3d88c45ae24180befa34883c44

--- develop/search/all_0.js
+++ develop/search/all_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['0_0',['AUIB_VALIDATION_LEVEL 0',['../md_docs_2AUI_01Boot.html#autotoc_md72',1,'']]]
+  ['0_0',['AUIB_VALIDATION_LEVEL 0',['../md_docs_2AUI_01Boot.html#autotoc_md71',1,'']]]
 ];
