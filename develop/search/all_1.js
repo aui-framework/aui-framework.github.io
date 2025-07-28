3f32f18e185f39883e195cebb30479cdf349d1ce
Alex2772 <Alex2772@users.noreply.github.com>
2025-07-28 15:18:14 +0000

    deploy: aui-framework/aui@fd145a4061c97d537e0d1d332f28994759d3f8df

--- develop/search/all_1.js
+++ develop/search/all_1.js
@@ -1,6 +1,6 @@
 var searchData=
 [
   ['1_0',['1',['../md_docs_2AUI_01Boot.html#autotoc_md72',1,'AUIB_VALIDATION_LEVEL 1'],['../md_docs_2AUI_01Boot.html#autotoc_md75',1,'Case 1']]],
-  ['1_20point_20minimal_20complexity_1',['1 Point - Minimal Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md155',1,'']]],
-  ['13_20points_20excessive_20complexity_2',['13+ Points - Excessive Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md161',1,'']]]
+  ['1_20point_20minimal_20complexity_1',['1 Point - Minimal Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md152',1,'']]],
+  ['13_20points_20excessive_20complexity_2',['13+ Points - Excessive Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md158',1,'']]]
 ];
