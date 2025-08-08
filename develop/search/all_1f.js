9236b575c7469d8029295ebb09acd9151094626d
Alex2772 <Alex2772@users.noreply.github.com>
2025-08-08 19:39:25 +0000

    deploy: aui-framework/aui@51cd2c9fce91d4279142a28aa1b6e038bd31bc35

--- develop/search/all_1f.js
+++ develop/search/all_1f.js
@@ -2,5 +2,5 @@ var searchData=
 [
   ['x11_0',['X11',['../structAPlatformAbstractionOptions_1_1X11.html',1,'APlatformAbstractionOptions']]],
   ['xmake_1',['Minimal UI Template XMake',['../example_minimal_ui_template_xmake.html',1,'']]],
-  ['xmake_20lua_2',['xmake.lua',['../example_minimal_ui_template_xmake.html#autotoc_md276',1,'']]]
+  ['xmake_20lua_2',['xmake.lua',['../example_minimal_ui_template_xmake.html#autotoc_md277',1,'']]]
 ];
