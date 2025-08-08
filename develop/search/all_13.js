c2cb38279d394f4ffef0c594a2e9601cdea34392
Alex2772 <Alex2772@users.noreply.github.com>
2025-08-08 02:53:33 +0000

    deploy: aui-framework/aui@3a24f714a5ee6020025871e429203171532d7b2a

--- develop/search/all_13.js
+++ develop/search/all_13.js
@@ -41,7 +41,7 @@ var searchData=
   ['life_38',['Game of Life',['../example_game_of_life.html',1,'']]],
   ['like_20a_20config_20file_20does_20not_20exist_20for_20your_20project_39',['&quot;looks like a config file does not exist for your project&quot;',['../md_docs_2AUI_01Boot.html#AFJKLF',1,'']]],
   ['like_20only_40',['like only',['../md_docs_2Runtime_01Dependency_01Resolution.html#RPATH',1,'Other platforms (UNIX-like only)'],['../md_docs_2Runtime_01Dependency_01Resolution.html#autotoc_md194',1,'Other platforms (UNIX-like only)']]],
-  ['limitations_41',['Limitations',['../md_docs_2Crosscompiling.html#autotoc_md162',1,'']]],
+  ['limitations_41',['Limitations',['../md_docs_2Crosscompiling.html#autotoc_md166',1,'']]],
   ['line_42',['Line',['../classATextLayoutHelper.html#ab31a0e199ad1f418b01083ae9de78531',1,'ATextLayoutHelper']]],
   ['line_43',['line',['../classIRenderer.html#a6a294680c11f3a3065e9a29c898322f7',1,'IRenderer']]],
   ['line_20arguments_44',['AUI Command Line Arguments',['../md_docs_2AUI_01command_01line_01arguments.html',1,'']]],
@@ -58,7 +58,7 @@ var searchData=
   ['list_55',['List',['../deprecated.html',1,'Deprecated List'],['../example_infinite_lazy_list.html',1,'Infinite Lazy List']]],
   ['list_56',['list',['../structAProcess_1_1ArgStringList.html#a6007a7ed88736a6f8fbe1f0b1d642c80',1,'AProcess::ArgStringList::list'],['../md_docs_2Troubleshoot_01list.html',1,'Troubleshoot list']]],
   ['listdir_57',['listDir',['../classAPath.html#af34c9fd2d0ff55d7c9177a108d8a10c5',1,'APath']]],
-  ['lists_58',['Always Use Trailing Comma in Initializer Lists',['../md_docs_2clang-format.html#autotoc_md123',1,'']]],
+  ['lists_58',['Always Use Trailing Comma in Initializer Lists',['../md_docs_2clang-format.html#autotoc_md126',1,'']]],
   ['lists_59',['Reactive lists',['../classAForEachUI.html#UIDeclarativeForTest_Reactive_lists',1,'']]],
   ['load_60',['load',['../classAProgramModule.html#aad786882f3306ae28642e2b2c7148393',1,'AProgramModule']]],
   ['loadexception_61',['LoadException',['../classAProgramModule_1_1LoadException.html',1,'AProgramModule']]],
@@ -70,6 +70,6 @@ var searchData=
   ['logwriter_67',['LogWriter',['../structALogger_1_1LogWriter.html',1,'ALogger']]],
   ['looks_20like_20a_20config_20file_20does_20not_20exist_20for_20your_20project_68',['&quot;looks like a config file does not exist for your project&quot;',['../md_docs_2AUI_01Boot.html#AFJKLF',1,'']]],
   ['loop_69',['loop',['../classAEventLoop.html#a0012f3f10188a0e93cf567c7018dae9e',1,'AEventLoop::loop()'],['../classIEventLoop.html#a790b37b3115b15f1224c85749d352d04',1,'IEventLoop::loop()'],['../classAScheduler.html#aab5b98be54a6068d22442b48d1c8f568',1,'AScheduler::loop()'],['../classAEmbedAuiWrap.html#a30230f25152db127b574b4e638fb01d8',1,'AEmbedAuiWrap::loop()'],['../classAWindowManager.html#a778f4e6eac766f892f29dfb44d69700a',1,'AWindowManager::loop()'],['../classIAudioPlayer.html#a670fb06519d7e606d2d757b670ed6287',1,'IAudioPlayer::loop()']]],
-  ['low_20complexity_70',['2 Points - Low Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md158',1,'']]],
+  ['low_20complexity_70',['2 Points - Low Complexity',['../md_docs_2Contributing_01to_01AUI.html#autotoc_md155',1,'']]],
   ['lua_71',['xmake.lua',['../example_minimal_ui_template_xmake.html#autotoc_md276',1,'']]]
 ];
